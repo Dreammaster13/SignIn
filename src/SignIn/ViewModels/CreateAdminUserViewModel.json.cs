90fe60e7a9f03d4763126aa24f11a80a77b037fd
Mihai <mihai@starcounter.com>
2017-09-06 01:17:15 -0700

    Fix failing checks

--- src/SignIn/ViewModels/CreateAdminUserViewModel.json.cs
+++ src/SignIn/ViewModels/CreateAdminUserViewModel.json.cs
@@ -12,14 +12,14 @@ namespace SignIn.ViewModels
         {
             this.Password = action.Value;
             this.Message = this.Data.GetPasswordValidationMessage();
-            this.IsAlert = !string.IsNullOrEmpty(this.Message);
+            this.IsAlert = (!(string.IsNullOrEmpty(this.Message)));
  
         }
         private void Handle(Input.PasswordRepeat action)
         {
             this.PasswordRepeat = action.Value;
             this.Message = this.Data.GetPasswordRepeatValidationMessage();
-            this.IsAlert = !string.IsNullOrEmpty(this.Message);
+            this.IsAlert = (!(string.IsNullOrEmpty(this.Message)));
         }
         private void Handle(Input.OkTrigger action)
         {
