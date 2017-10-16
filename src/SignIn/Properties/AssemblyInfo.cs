e2b15fd35866a81006d47f1bae2ebbe02a5a6c4d
habib535 <habib535@gmail.com>
2017-10-16 13:32:39 +0200

    6.1.0

--- src/SignIn/Properties/AssemblyInfo.cs
+++ src/SignIn/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Starcounter.Internal;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.0")]
-[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyVersion("6.1.0")]
+[assembly: AssemblyFileVersion("6.1.0")]
 
 
 // Starcounter Package information
