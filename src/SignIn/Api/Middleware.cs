8ca9b36ea6e2bbf1b2142dbdb1c01b05adcc7a11
Mihai <mihai@starcounter.com>
2018-01-09 14:31:34 +0100

    Remove ImplicitStandaloneProvider

--- src/SignIn/Api/Middleware.cs
+++ src/SignIn/Api/Middleware.cs
@@ -12,51 +12,8 @@ namespace SignIn.Api
 
         internal void Register()
         {
-            #region Implicit standalone page template
-            const string ImplicitStandaloneTemplate = @"<!DOCTYPE html>
-<html>
-<head>
-    <meta charset=""utf-8"">
-    <meta name=""viewport"" content=""width=device-width, initial-scale=1"">
-    <title>{0}</title>
-    <script src=""/sys/webcomponentsjs/webcomponents-lite.min.js""></script>
-    <script>
-      /* this script must run before Polymer is imported */
-      /*
-       * Let Polymer use native Shadow DOM if available.
-       * Otherwise (at least Polymer 1.x) assumes everybody else
-       * uses ShadyDOM, which is not true, as many Vanilla CE uses
-       * real Shadow DOM.
-       */
-      window.Polymer = {{
-        dom: ""shadow""
-      }};
-    </script>
-    <link rel=""import"" href=""/sys/polymer/polymer.html"">
-    <link rel=""import"" href=""/sys/starcounter.html"">
-    <link rel=""import"" href=""/sys/starcounter-include/starcounter-include.html"">
-    <link rel=""import"" href=""/sys/bootstrap.html"">
-    <style>
-        body {{
-            margin: 20px;
-        }}
-        body > starcounter-include{{
-            height: 100%;
-        }}
-    </style>
-</head>
-<body>
-    <dom-bind id=""puppet-root"">
-    <template is=""dom-bind"">
-        <starcounter-include view-model=""{{{{model}}}}""></starcounter-include>
-    </template>
-</dom-bind>
-    <puppet-client ref=""puppet-root"" remote-url=""{1}""></puppet-client>
-</body>
-</html>";
-            #endregion
             Application.Current.Use(new HtmlFromJsonProvider());
-            Application.Current.Use(new PartialToStandaloneHtmlProvider(ImplicitStandaloneTemplate));
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
 
             Application.Current.Use((Request req) =>
             {
