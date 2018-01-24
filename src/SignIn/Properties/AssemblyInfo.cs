47386551940212bb1520ebe29fe18af12ccb9be6
Mihai <mihai@starcounter.com>
2018-01-24 19:41:39 +0100

    6.1.1

--- src/SignIn/Properties/AssemblyInfo.cs
+++ src/SignIn/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Starcounter.Internal;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.1.0")]
-[assembly: AssemblyFileVersion("6.1.0")]
+[assembly: AssemblyVersion("6.1.1")]
+[assembly: AssemblyFileVersion("6.1.1")]
 
 
 // Starcounter Package information
