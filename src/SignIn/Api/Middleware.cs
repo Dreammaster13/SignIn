e1d14e1e22bf9b17114754e07d1482258056d63d
Dimitris Thanasis <Dimitris.th@hotmail.com>
2018-02-21 11:05:18 +0100

    Removed Bootstrap

--- src/SignIn/Api/Middleware.cs
+++ src/SignIn/Api/Middleware.cs
@@ -12,55 +12,8 @@ namespace SignIn.Api
 
         internal void Register()
         {
-            #region Implicit standalone page template
-            const string ImplicitStandaloneTemplate = @"<!DOCTYPE html>
-<html>
-<head>
-    <!-- hello! This is to verify that I was loaded from StarcounterClientFiles -->
-    <meta charset=""utf-8"">
-    <title>{0}</title>
-
-    <script src=""/sys/webcomponentsjs/webcomponents-lite.js""></script>
-    <script>
-      /* this script must run before Polymer is imported */
-      /*
-       * Let Polymer use native Shadow DOM if available.
-       * Otherwise (at least Polymer 1.x) assumes everybody else
-       * uses ShadyDOM, which is not true, as many Vanilla CE uses
-       * real Shadow DOM.
-       */
-      window.Polymer = {{
-        dom: ""shadow""
-      }};
-    </script>
-    <link rel=""import"" href=""/sys/polymer/polymer.html"">
-    <link rel=""import"" href=""/sys/starcounter.html"">
-    <link rel=""import"" href=""/sys/starcounter-include/starcounter-include.html"">
-
-    <link rel=""import"" href=""/sys/bootstrap.html"">
-    <style>
-        body {{
-            margin: 20px;
-        }}
-        body > starcounter-include{{
-            height: 100%;
-        }}
-    </style>
-</head>
-<body>
-    <dom-bind id=""palindrom-root"">
-        <template is=""dom-bind"">
-            <starcounter-include view-model=""{{{{model}}}}""></starcounter-include>
-        </template>
-    </dom-bind>
-    
-    <palindrom-client ref=""palindrom-root"" remote-url=""{1}""></palindrom-client>
-</body>
-</html>
-";
-            #endregion
             Application.Current.Use(new HtmlFromJsonProvider());
-            Application.Current.Use(new PartialToStandaloneHtmlProvider(ImplicitStandaloneTemplate));
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
 
             Application.Current.Use((Request req) =>
             {
